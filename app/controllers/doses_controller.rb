b1f0409c3a7590f65d80b8e592cb500d28d7101d
Daniella Depaz <danielladcoding@protonmail.com>
2019-02-14 17:17:13 -0500

    In Progress

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,50 @@
+class DosesController < ApplicationController
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  # read
+  def index
+    @doses = Dose.all
+  end
+
+  def show
+    @dose = Dose.find(params[:id])
+  end
+
+  # update
+  def edit
+    @dose = Dose.find(params[:id])
+  end
+
+  def update
+    @dose = Dose.find(params[:id])
+    @dose.update(dose_params)
+    redirect_to index_path
+  end
+
+  # delete
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+  end
+end
