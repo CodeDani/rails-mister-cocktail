bb713ee2d7fb56e1be58c82b9faf0d833c738b53
Daniella Depaz <danielladcoding@protonmail.com>
2019-02-14 17:33:53 -0500

    In Progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,27 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'open-uri'
-require 'json'
-
-Ingredient.destroy_all
-
-list = JSON.parse(open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read)
-list["drinks"].each do |ingredient|
-  Ingredient.create(name: ingredient["strIngredient1"])
-end
-
-Cocktail.create!(
-  name: 'AppleTini'
-)
-
-Cocktail.create!(
-  name: 'Tequila Sunrise'
-)
-
-Cocktail.create!(
-  name: 'Chocolate Martini'
-)
-Cocktail.create!(
-  name: 'Sangria'
-)
